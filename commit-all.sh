e838619ed529bf9b6a8712f040a22ba2b28c3197
Daniel Melo <danielmelogpi@gmail.com>
2017-08-23 16:09:22 -0700

    new notes

--- commit-all.sh
+++ commit-all.sh
@@ -3,6 +3,6 @@
 git add --all
 git commit -m "new notes"
 git push origin master
-echo "\033[0;32m"
-echo "\n\nNotes sent to upstream repo"
-echo "\033[0m"
+printf "\033[0;32m"
+printf "\n\nNotes sent to upstream repo"
+printf "\033[0m"
